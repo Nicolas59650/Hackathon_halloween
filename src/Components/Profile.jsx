211baf7875d39ac4f2131c6fb4db2496a58818ca
Emma Kimpe <emmaemobaka@gmail.com>
2018-10-30 18:11:03 +0100

    Addings Footer, Header and Router to Hackaton project.

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <p> Look at your profile and custom your own avatar !</p>
+    </div>
+  );
+};
+
+export default Profile;
