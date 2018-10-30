27551042473270781e23ba114042031386018268
Steven AD <s.dailliez@gmail.com>
2018-10-30 14:49:27 +0100

    Added background and button for Homepage.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Homepage from "./components/Homepage";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div className="App-Body mt-5 p-5">
+        <button className="button-homepage">
+          <span>Que la chasse commence !</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
